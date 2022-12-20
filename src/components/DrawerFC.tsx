f3f39a15cd41b7ebec51da89adb8abb97b6918f6
yura12481 <yura124812481@gmail.com>
2022-12-20 11:25:26 +0200

    add

---
+++ src/components/DrawerFC.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import {
+  Drawer,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+const pages = [
+  'Home',
+  'Profile',
+  'Basket',
+  'About',
+  'Shop',
+  'Page',
+  'Block',
+  'Contact',
+];
+
+const DrawerFC: React.FC = () => {
+  const [openDrawer, setOpenDrawer] = useState<boolean>(false);
+  return (
+    <React.Fragment>
+      <Drawer
+        open={openDrawer}
+        onClose={() => setOpenDrawer(false)}
+        anchor="right"
+      >
+        <List sx={{ width: '250px' }}>
+          {pages.map((page) => (
+            <ListItemButton onClick={() => setOpenDrawer(false)} key={page}>
+              <ListItemIcon>
+                <ListItemText>{page}</ListItemText>
+              </ListItemIcon>
+            </ListItemButton>
+          ))}
+        </List>
+      </Drawer>
+      <IconButton
+        sx={{ marginLeft: 'auto' }}
+        size="large"
+        color="primary"
+        onClick={() => setOpenDrawer(!openDrawer)}
+      >
+        <MenuIcon />
+      </IconButton>
+    </React.Fragment>
+  );
+};
+
+export default DrawerFC;
---
+++ src/components/Header.tsx
@@ -0,0 +1,149 @@
+import React, { useState } from 'react';
+import {
+  AppBar,
+  Container,
+  IconButton,
+  Tab,
+  Tabs,
+  Toolbar,
+  Typography,
+  Badge,
+  Link,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
+import StoreIcon from '@mui/icons-material/Store';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import DrawerFC from './DrawerFC';
+
+const pages = ['Home', 'About', 'Shop', 'Page', 'Block', 'Contact'];
+
+const Header: React.FC = () => {
+  const [value, setValue] = useState<number>(0);
+  const theme = useTheme();
+  const media = useMediaQuery(theme.breakpoints.down('lg'));
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        background: '#DEECEC',
+        height: '100px',
+        borderBottom: '2px solid #84BABE',
+      }}
+    >
+      <Container maxWidth="lg">
+        <Toolbar
+          sx={{
+            margin: '15px 0 15px 0',
+            '&.MuiToolbar-root': { padding: '0' },
+          }}
+        >
+          {media ? (
+            <>
+              <IconButton size="large" color="primary">
+                <Link href="/">
+                  <StoreIcon />
+                </Link>
+              </IconButton>
+              <Link
+                href="/"
+                underline="hover"
+                sx={{
+                  '&.MuiTypography-root': {
+                    color: '#589195',
+                  },
+                }}
+              >
+                <Typography
+                  variant="h2"
+                  sx={{
+                    fontFamily: "'Cormorant', serif",
+                    fontWeight: 700,
+                    fontSize: '30px',
+                    lineHeight: '36px',
+                    color: '#589195',
+                  }}
+                >
+                  All Year Shop
+                </Typography>
+              </Link>
+              <DrawerFC />
+            </>
+          ) : (
+            <>
+              <IconButton size="large" color="primary">
+                <Link href="/">
+                  <StoreIcon />
+                </Link>
+              </IconButton>
+              <Link
+                href="/"
+                underline="hover"
+                sx={{
+                  '&.MuiTypography-root': {
+                    color: '#589195',
+                  },
+                }}
+              >
+                <Typography
+                  variant="h2"
+                  sx={{
+                    fontFamily: "'Cormorant', serif",
+                    fontWeight: 700,
+                    fontSize: '30px',
+                    lineHeight: '36px',
+                    color: '#589195',
+                  }}
+                >
+                  All Year Shop
+                </Typography>
+              </Link>
+              <Tabs
+                onChange={(e, value) => setValue(value)}
+                value={value}
+                textColor="inherit"
+                sx={{
+                  maxWidth: '607px',
+                  marginLeft: 'auto',
+                }}
+              >
+                {pages.map((page) => (
+                  <Tab
+                    key={page}
+                    label={page}
+                    sx={{
+                      color: '#6A7087',
+                      fontFamily: "'Work Sans', sans-serif",
+                      fontWeight: 400,
+                      fontSize: '18px',
+                      lineHeight: '21.1px',
+                    }}
+                  />
+                ))}
+              </Tabs>
+              <IconButton
+                size="large"
+                color="primary"
+                sx={{ marginLeft: 'auto' }}
+              >
+                <SearchIcon />
+              </IconButton>
+              <IconButton size="large" color="primary">
+                <PersonIcon />
+              </IconButton>
+              <IconButton>
+                <Badge badgeContent="6" color="error">
+                  <ShoppingCartIcon color="primary" />
+                </Badge>
+              </IconButton>
+            </>
+          )}
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+
+export default Header;
